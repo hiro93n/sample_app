710c8fb1419d6be44113728f756403631c213019
hiro93n <hiro93n@gmail.com>
2014-09-23 23:03:41 +0900

    full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+# ページ毎の完全なタイトルを返す
+ def full_title(pages_title)
+  base_title = "Ruby on Rails Tutrial Sample App"
+	if pages_title empty?
+		base_title
+	else
+		"#{base_title}｜#{pages_title}"
+   end
+ end
 end
