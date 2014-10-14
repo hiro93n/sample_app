d4c28c9a8569006122fbfeda1a72f15c031e2798
yamaguchi-takahiro <yamaguchi.takahiro@O-02871-MAC.local>
2014-10-14 19:56:34 +0900

    9_1_1

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -25,5 +25,17 @@ describe "Autgentication" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
   　end
+
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user }
+
+      it { should have_title(user.name) }
+      it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
+      it { should have_link('Sign out',    href: signout_path) }
+      it { should_not have_link('Sign in', href: signin_path) }
+    end
+
 	end
 end
